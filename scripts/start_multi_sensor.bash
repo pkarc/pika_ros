2bdc63d66bb6cc791a47b9e2aa45e39a45fbe38c
Ivan Jaramillo <pkarc@pkarc.dev>
2026-01-03 14:51:56 -0600

    add .gitignore

--- scripts/start_multi_sensor.bash
+++ scripts/start_multi_sensor.bash
@@ -3,8 +3,8 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 camera_fps=30
 camera_width=640
 camera_height=480
-l_depth_camera_no=230322272110
-r_depth_camera_no=230322270988
+l_depth_camera_no=335122272049
+r_depth_camera_no=323622271191
 
 l_serial_port=/dev/ttyUSB50
 r_serial_port=/dev/ttyUSB51
@@ -13,7 +13,7 @@ l_fisheye_port=50
 r_fisheye_port=51
 sudo chmod a+rw /dev/video*
 
-source /opt/ros/humble/setup.bash && cd $SCRIPT_DIR/../install/sensor_tools/share/sensor_tools/scripts/ && chmod 777 usb_camera.py
+    source /opt/ros/jazzy/setup.bash && cd $SCRIPT_DIR/../install/sensor_tools/share/sensor_tools/scripts/ && chmod 777 usb_camera.py
 if [ -n "$1" ]; then
     source $SCRIPT_DIR/../install/setup.bash && ros2 launch sensor_tools open_multi_sensor.launch.py l_depth_camera_no:=_$l_depth_camera_no r_depth_camera_no:=_$r_depth_camera_no l_serial_port:=$l_serial_port r_serial_port:=$r_serial_port l_fisheye_port:=$l_fisheye_port r_fisheye_port:=$r_fisheye_port camera_fps:=$camera_fps camera_width:=$camera_width camera_height:=$camera_height camera_profile:=$camera_width,$camera_height,$camera_fps name:=$1 name_index:=$1_
 else
